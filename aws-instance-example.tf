2d6311de3af10ab52dcaaed7d2067645622b42ea
GarvitKushwaha <66833979+GarvitKushwaha@users.noreply.github.com>
2021-11-22 15:12:56 +0530

    Update aws-instance-example.tf

--- aws-instance-example.tf
+++ aws-instance-example.tf
@@ -10,9 +10,9 @@ resource "aws_instance" "web1" {
       sudo amazon-linux-extras install ansible2 -y
       sudo yum install git -y
       sudo yum install maven -y      
-      git -C ./home/ec2-user clone https://github.com/Abishek-Ravichander/Terra-Ansi-jFrog.git          
-      cd /home/ec2-user/Terra-Ansi-jFrog && ansible-playbook main.yml -f 10
-      cd /home/ec2-user/Terra-Ansi-jFrog && wget -O Java-Ansible.war https://abishek.jfrog.io/artifactory/abi_new/MyMavanWebapp/Java-Ansible/0.0.1-SNAPSHOT/Java-Ansible-0.0.1-SNAPSHOT.war
-      cd /home/ec2-user/Terra-Ansi-jFrog && ansible-playbook deploy.yml -f 10
+      git -C ./home/ec2-user clone https://github.com/GarvitKushwaha/Terraform.git          
+      cd /home/ec2-user/Terraform && ansible-playbook main.yml -f 10
+      cd /home/ec2-user/Terraform && wget -O Java-Ansible.war https://abishek.jfrog.io/artifactory/abi_new/MyMavanWebapp/Java-Ansible/0.0.1-SNAPSHOT/Java-Ansible-0.0.1-SNAPSHOT.war
+      cd /home/ec2-user/Terrafrom && ansible-playbook deploy.yml -f 10
       EOF
    }
