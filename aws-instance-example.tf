ef437939912d784a5f3182c661057a63b388b69e
GarvitKushwaha <66833979+GarvitKushwaha@users.noreply.github.com>
2021-11-22 16:15:29 +0530

    Update aws-instance-example.tf

--- aws-instance-example.tf
+++ aws-instance-example.tf
@@ -13,6 +13,6 @@ resource "aws_instance" "web1" {
       git -C ./home/ec2-user clone https://github.com/GarvitKushwaha/Terraform.git          
       cd /home/ec2-user/Terraform && ansible-playbook main.yml -f 10
       cd /home/ec2-user/Terraform && wget -O Java-Ansible.war https://abishek.jfrog.io/artifactory/abi_new/MyMavanWebapp/Java-Ansible/0.0.1-SNAPSHOT/Java-Ansible-0.0.1-SNAPSHOT.war
-      cd /home/ec2-user/Terrafrom && ansible-playbook deploy.yml -f 10
+      cd /home/ec2-user/Terraform && ansible-playbook deploy.yml -f 10
       EOF
    }
